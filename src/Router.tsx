d56d838e695aca6c1c1a1ed64255f9839e2efc3f
donggyun <donggyunkim12@gmail.com>
2024-04-08 14:40:10 +0900

    chore: 모든 라우팅 페이지 가운데 정렬

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Home } from '@pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AppWrapper>
     </BrowserRouter>
   );
 }
 
 export default Router;
+
+const AppWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
