1e411887d71f18acbf78af74ccac6d6d7489fb15
donggyun <donggyunkim12@gmail.com>
2024-04-10 16:19:21 +0900

    feat(WishListPage): wishList에 따른 전시회 전체리스트 filter 구현

--- src/components/WishList/index.tsx
+++ src/components/WishList/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from '@components/Home';
 import Footer from '@components/common/Footer';
 import ExhibitionList from '@components/common/Exhibition/ExhibitionList';
+import { WishListContext } from '@src/store/wishList';
+import { useExhibitionList } from '@hooks/useExhibition';
+import LoadingSpinner from '@components/common/LoadingSpinner';
 
 const WishListPage = () => {
+  const { list, isLoading } = useExhibitionList();
+  const { wishList } = useContext(WishListContext);
+
   return (
     <Container>
-      <ExhibitionList list={[]} />
+      {isLoading ? (
+        <LoadingSpinner width={40} height={40} $borderWidth={3} />
+      ) : (
+        <ExhibitionList
+          list={list.filter((item) => wishList.find((wishItemId) => wishItemId === item.id))}
+        />
+      )}
       <Footer />
     </Container>
   );
