60b519c44b57a3f016d2e9da73c0333591a3cccf
donggyun <donggyunkim12@gmail.com>
2024-04-11 10:52:48 +0900

    feat(useExhibition): 전시회 아이템 API 훅 구현

--- src/hooks/useExhibition.ts
+++ src/hooks/useExhibition.ts
@@ -1,6 +1,7 @@
 import { ExhibitionItemType } from '../types/exhibition';
 import { useState, useEffect } from 'react';
-import { getApiExhibitionList } from '@src/apis';
+import { getApiExhibitionList, getDetailExhibition } from '@src/apis';
+import { useNavigate } from 'react-router-dom';
 
 export const useExhibitionList = () => {
   const [list, setList] = useState<ExhibitionItemType[]>([]);
@@ -19,4 +20,28 @@ export const useExhibitionList = () => {
   }, []);
 
   return { list, isLoading };
+};
+
+export const useExhibitionItem = (id: number) => {
+  const navigate = useNavigate();
+
+  const [item, setItem] = useState<ExhibitionItemType>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const promise = async () => {
+      setIsLoading(true);
+      try {
+        const res = await getDetailExhibition({ id });
+        if (res) setItem(res);
+      } catch (error) {
+        console.error(error);
+        navigate('/404');
+      }
+      setIsLoading(false);
+    };
+    promise();
+  }, []);
+
+  return { item, isLoading };
 };
